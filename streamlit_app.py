7b9ef38be383934668a3708a0437e5e122477de4
Yuyao Huang <106633493+huangyuyao@users.noreply.github.com>
2024-07-10 19:03:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ hide_streamlit_style = """
                     </style>
                     """
                     
-        st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
 # Create database connection and tables
